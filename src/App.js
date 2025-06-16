b948454747920a03ffdf9bb5277f68622b5751fd
Kavin-crew <abreganakavin@gmail.com>
2025-06-17 00:21:45 +0800

    init files

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "747681889";
+  let ProductId = "AEWAL-001";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
